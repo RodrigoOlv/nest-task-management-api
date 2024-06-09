c014c29e81eb90bb022a176ef9234ed3d6a4df76
Rodrigo Leite de Oliveira <rodrigo.oliveira@grupo-eagle.com>
2024-06-09 19:14:06 -0300

    - Add delete method - Add findAll aka search method - Add class documentation

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,6 +1,41 @@
+/**
+ * Data Transfer Object (DTO) para representar uma tarefa.
+ * Contém as propriedades necessárias para definir uma tarefa.
+ */
 export class TaskDto {
+    /**
+     * Identificador único da tarefa.
+     */
     id: string;
+
+    /**
+     * Título da tarefa.
+     */
     title: string;
+
+    /**
+     * Descrição detalhada da tarefa.
+     */
     description: string;
+
+    /**
+     * Data de expiração da tarefa.
+     */
     expirationDate: Date;
+}
+
+/**
+ * Interface para os parâmetros de busca de tarefas.
+ * Permite a filtragem de tarefas por título e descrição.
+ */
+export interface FindAllParameters {
+    /**
+     * Título das tarefas a serem buscadas.
+     */
+    title: string;
+
+    /**
+     * Descrição das tarefas a serem buscadas.
+     */
+    description: string;
 }
